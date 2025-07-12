3df8bc0990469ae18bb3d17d301d69f898f9c2d9
tomaszaba <tomas.zaba@outlook.com>
2025-07-12 21:18:05 +0200

    add a roxygen skeleton for data documentation

---
+++ R/data.R
@@ -0,0 +1,32 @@
+#' 
+#' Sample dataset
+#' 
+#' @description
+#' lorem
+#' 
+#' @format lorem
+#' 
+#' @source anonymous
+#' 
+#' @examples
+#' anthro
+#' 
+"anthro"
+
+
+#'
+#' 
+#' 
+#' Sample data 
+#' 
+#' @description
+#' lorem
+#' 
+#' @source anonymous 
+#' 
+#' @format lorem
+#' 
+#' @examples 
+#' df
+#' 
+"df"
