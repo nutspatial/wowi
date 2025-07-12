60f715a3ef5be272bc06d541f70b466045d11990
tomaszaba <tomas.zaba@outlook.com>
2025-07-12 18:28:55 +0200

    declare dependencies

---
+++ R/wowi-package.R
@@ -0,0 +1,28 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr arrange
+#' @importFrom dplyr case_when
+#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
+#' @importFrom dplyr relocate
+#' @importFrom dplyr rename
+#' @importFrom dplyr select
+#' @importFrom dplyr slice_head
+#' @importFrom mwana define_wasting
+#' @importFrom mwana mw_wrangle_age
+#' @importFrom mwana mw_wrangle_muac
+#' @importFrom mwana mw_wrangle_wfhz
+#' @importFrom mwana recode_muac
+#' @importFrom rlang .data
+#' @importFrom rlang parse_expr
+#' @importFrom rlang sym
+#' @importFrom rsatscan satscan
+#' @importFrom rsatscan ss.options
+#' @importFrom rsatscan write.cas
+#' @importFrom rsatscan write.ctl
+#' @importFrom rsatscan write.geo
+#' @importFrom rsatscan write.ss.prm
+## usethis namespace: end
+NULL
