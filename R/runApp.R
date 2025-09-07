1946cb3c3c84a7cc44446e9be8424253650ad183
tomaszaba <tomas.zaba@outlook.com>
2025-09-07 19:17:27 +0200

    document app

--- R/runApp.R
+++ R/runApp.R
@@ -1,8 +1,21 @@
-run_wowi <- function() {
+#'
+#' 
+#' Initialise built-in Shiny application
+#' 
+#' @return NULL
+#' 
+#' @examples
+#' if(interactive()) ww_run_app()
+#' 
+#' @export
+#' 
+ww_run_app <- function() {
   app_dir <- system.file("wowi", "app", package = "wowi")
 
   if (app_dir == "") {
-    stop("Could not find Shiny directory. Try re-installing `wowi`.", call = FALSE)
+    stop(
+      "Could not find Shiny directory. Try re-installing `wowi`.", call = FALSE
+    )
   }
 
   shiny::runApp(appDir = app_dir, display.mode = "normal")
