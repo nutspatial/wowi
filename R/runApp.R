c9109c0adf9baf6ca11875e23093d3ae7dd63543
tomaszaba <tomas.zaba@outlook.com>
2025-09-09 00:05:47 +0200

    fix Codecov

--- R/runApp.R
+++ R/runApp.R
@@ -1,5 +1,21 @@
 #'
 #' 
+#' Helper function to locate app directory
+#' 
+#' @keywords internal
+#' 
+get_app_dir <- function() {
+  app_dir <- system.file("wowi", "app", package = "wowi")
+  if (app_dir == "") {
+    stop("Could not find Shiny directory. Try re-installing `wowi`.", call. = FALSE)
+  }
+  app_dir
+}
+
+
+
+#'
+#' 
 #' Initialise built-in Shiny application
 #' 
 #' @return NULL
@@ -8,15 +24,9 @@
 #' if(interactive()) ww_run_app()
 #' 
 #' @export
-#' 
+
+# nocov start
 ww_run_app <- function() {
-  app_dir <- system.file("wowi", "app", package = "wowi")
-
-  if (app_dir == "") {
-    stop(
-      "Could not find Shiny directory. Try re-installing `wowi`.", call = FALSE
-    )
-  }
-
-  shiny::runApp(appDir = app_dir, display.mode = "normal")
+  shiny::runApp(appDir = get_app_dir(), display.mode = "normal")
 }
+# nocov end
