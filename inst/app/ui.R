81b05eeb2bd0033d08137652dc1d4605935842f1
tomaszaba <tomas.zaba@outlook.com>
2025-11-22 12:07:45 +0200

    user guide - Home tab

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -58,7 +58,7 @@ ui <- tagList(
       # Bootstrap 5 icon name
       layout_sidebar(
         sidebar = tags$div(
-          style = "padding: 1rem;",
+          style = "padding: 1rem; align-items: end;",
           tags$h4("Contents"),
           tags$h6(tags$a(href = "#sec1", "Welcome")),
           tags$h6(tags$a(href = "#sec2", "Data Upload")),
@@ -69,10 +69,9 @@ ui <- tagList(
           tags$h6(tags$a(href = "#sec7", "Useful Resources"))
         ),
         card(
-          style = "padding: 1rem; background-color: #fbfdfd;",
+          style = "background-color: #fbfdfd;",
           tags$html(
             tags$div(
-              style = "padding: 0.5rem 1rem;",
 
               ### Title + Logo row ----
               tags$div(
@@ -80,7 +79,7 @@ ui <- tagList(
 
                 #### Title ----
                 tags$h3(
-                  style = "margin: 0; font-weight: bold;",
+                  style = "margin: 0; font-weight: bold; line-height: 1.2", "Title",
                     "
                     Detecting Statistically Significant Spatial Clusters of 
                     Acute Malnutrition
@@ -100,42 +99,44 @@ ui <- tagList(
               ),
 
               ### Subtitle directly below, no spacing ----
-              tags$h4(
-                style = "margin: 0; font-weight: normal; line-height: 1.2;",
+              tags$p(tags$h4(
+                style = "font-weight: normal; align-items: flex-start;", "Subtitle",
                 list(
                   "A simplified workflow of the ", tags$code("wowi"), " package for non-R users"
                 )
               )
+            )
             ),
             tags$hr(),
             tags$div(
               id = "sec1",
-              style = "text-align: justify;",
+              style = "text-align: justify; gap; 0 !important;",
               tags$p(
                 "
-                This app is part of the", tags$code("wowi"), "package, designed for
-                non-R users.", "The", tags$code("wowi"), "package is a set of utility
-                functions for detecting spatial clusters - whether high-only or high
-                and low rates - of acute malnutrition that are unlikely to be ocurring by
-                chance. These clusters are identified using SaTScan's Bernoulli
-                spatial-scan model.
+                This app is a lightweight, field-ready and handy tool thoughtful
+                conceived to help users detect spatial clusters - whether 
+                high-only or high and low rates - of acute malnutrition that are
+                unlikely to be ocurring by chance. It automates the key analysis
+                workflow of the package", tags$code("wowi"), "for non-R users.
                 "
+                
               ),
               tags$p(
                 "
                 The app only does anything useful if you have SaTScan installed on
-                your computer, and if the", tags$code("wowi"), "package is installed in R.
+                your computer, and if the", tags$code("mwana"), "package is installed in R.
                 "
               ),
               tags$p(
                 "
-                  The app is divided into three easy-to-manage tabs, as described below:
+                  The app is divided into three easy-to-navigate tabs, apart from 
+                  the Home - where you are now:
                   "
               ),
               tags$ol(
-                tags$li("Data Upload"),
-                tags$li("Data Wrangling"),
-                tags$li("Spatial Scan")
+                tags$li(tags$b("Data Upload")),
+                tags$li(tags$b("Data Wrangling")),
+                tags$li(tags$b("Spatial Scan"))
               )
             ),
             tags$hr(),
@@ -143,30 +144,51 @@ ui <- tagList(
             #### Data uploading ----
             tags$div(
               id = "sec2",
-              style = "text-align: justify;",
-              tags$p(tags$b("Data Upload")),
+              style = "text-align: justify; gap; 0 !important;",
+              tags$p(tags$b("1. Data Upload")),
               tags$p(
                 "
-                In this tab, you are expected to upload the input dataset in
-                comma-separated format (CSV). Only this format is accepted.
+                This is where the workflow begins. Upload the dataset saved in a
+                comma-separated-value format (.csv); this is the only accepted 
+                format. Click on the 'Browse' button to locate the file to be 
+                uploaded from your computer; it is as simple as that. 
+                Once uploaded, the first 20 rows will be priviewed on the right side.
                 "
               ),
               tags$p(
-                tags$b(
                   tags$ul(
-                    tags$li("Required variables"),
+                    tags$li(tags$b("Data requirements")),
+                    tags$p(
+                      "
+                        The data to be uploaded must have been tidy up in accordance
+                        to the below-described app's", tags$b("input file"), "and",
+                        tags$b("input variable"), "requirements:
+                        "
+                    ),
                     tags$br(),
                     tags$ul(
-                      tags$li("Aspatial Variables"),
+                      tags$li(tags$b("Input file requirements"), 
+                      tags$p(
+                        tags$b("File naming:"), "the file name must use
+                            underscore ( _ ) to separate words. Hyphen ( - ) or
+                            simple spaces will lead to errors along the uploading
+                            process. Consider the following naming example:",
+                              tags$em("my_file_to_upload.csv")
+                        )
+                    ),
+
+                    tags$br(),
+                      tags$li(tags$b("Aspatial Variables")),
                       tags$span(
                         style = "font-weight: normal; space-between;",
                         "
-                        Acute malnutrition can be defined based on weight-for-heigth
-                        z-scores (WFHZ), based on Mid-Upper Arm Circumference (MUAC)
-                        or based on the combination of the former two, including or
-                        not bilateral oedema. The required variables to be uploaded
-                        will depend on the method to be considered first and
-                        foremost. Nonetheless, all in all:
+                        Acute malnutrition can be defined based on either 
+                        Weight-for-Heigth z-scores (WFHZ) or Mid-Upper Arm 
+                        Circumference (MUAC), or even based on the combination 
+                        of the former two - including or not bilateral oedema. 
+                        On this note, the required variables to be uploaded
+                        will depend on the method to be considered in the first 
+                        place. Nonetheless, all in all:
                         "
                       ),
                       tags$ul(
@@ -176,39 +198,47 @@ ui <- tagList(
                           tags$li(
                             tags$b("Age:"),
                             "
-                            must be in months. Any values outside the
+                            values must be in months. Any values outside the
                             range of 6 to 59 months old will be set as 'not
                             applicable'. The variable name must be written in
-                            lowercase (age).
+                            lowercase ('age').
                             "
                           ),
                           tags$li(
                             tags$b("Sex:"),
                             "
-                            must be code as 1 for boys/male and 2 for
+                            values must be given in 'm' for boys/male and 'f' for
                             girls/female. The variable name must be written in lowercase
-                            (sex).
+                            ('sex').
                             "
                           ),
                           tags$li(
-                            tags$b("MUAC:"), "must be in centimetres."
+                            tags$b("MUAC:"), 
+                            "
+                            values must be given in millimetres. Ensure that there
+                            no strange numbers, such as '114.1'. The presence of
+                            decimal places (even if one case) will raise error
+                            in the data wrangling tab and hault the app.
+                            "
                           ),
                           tags$li(
-                            tags$b("Weight:"), "must be in Kilograms."
+                            tags$b("Weight:"), "values must be given in Kilograms."
                           ),
                           tags$li(
-                            tags$b("Height:"), "must be in centimetres."
+                            tags$b("Height:"), "values must be given in centimetres."
                           ),
                           tags$li(
-                            tags$b("Oedema:"), "must be coded as 'y' for yes and 'n'
-                       for no."
+                            tags$b("Oedema:"), 
+                            "
+                            values must be given in 'y' for yes and 'n' for no.
+                            "
                           )
                         )
                       )
                     ),
                     tags$br(),
                     tags$ul(
-                      tags$li("Spatial Variables"),
+                      tags$li(tags$b("Spatial Variables")),
                       tags$div(
                         style = "font-weight: normal;",
                         tags$ul(
@@ -219,25 +249,16 @@ ui <- tagList(
                             tags$b("Longitude: y-axis")
                           )
                         )
-                      ),
-                      br(),
-                      tags$p(
-                        style = "font-weight: normal",
-                        "
-                        Once uploaded, the file becomes available for use in the
-                        following tab.
-                        "
                       )
                     )
                   )
-                )
               ),
               tags$hr(),
 
               #### Data wrangling ----
               tags$div(
                 id = "sec3",
-                style = "text-align: justify;",
+                style = "text-align: justify; gap; 0 !important;",
                 tags$p(tags$b("Data Wrangling")),
                 tags$p(
                   "
@@ -252,13 +273,7 @@ ui <- tagList(
                   provided below.
                   "
                 ),
-                tags$p("In this process, the variable oedema is optional."),
-                tags$p(
-                  "
-                  Once wrangled, the data becomes available for use in the following
-                  tab.
-                  "
-                )
+                tags$p("In this process, the variable oedema is optional.")
               )
             ),
             tags$hr(),
@@ -266,7 +281,7 @@ ui <- tagList(
             #### Run Spatial Scan ----
             tags$div(
               id = "sec4",
-              style = "text-align: justify;",
+              style = "text-align: justify; gap; 0 !important;",
               tags$p(tags$b("Spatial Scan")),
               tags$p(
                 "
@@ -275,11 +290,11 @@ ui <- tagList(
                 dataset contains only one area (e.g., district, county), and the
                 scan should be run within that area. Multiple-area analysis applies
                 when your dataset includes several areas, and the scan should be
-                run across them. If conducting a single-area analysis, enter the
+                run across them. If the former scope is chosen, enter the
                 name of the area under review in the corresponding input field.
-                Otherwise, for multiple-area analysis, specify the variable in your
+                Otherwise, specify the variable in your
                 dataset that contains the area names. Once complete, fill in the
-                remaining fields as appropriate, then click Run Scan to initiate
+                remaining fields as appropriate, then click 'Run Scan' to initiate
                 the process.
                "
               ),
@@ -287,9 +302,9 @@ ui <- tagList(
                 "
                 Once the scan is complete, several files will be saved in the
                 directory you specified earlier. A list of these files will appear
-                under the 'Created Files' section. Additionally, the 'Summary Results
+                under the 'Created Files' section. Additionally, the 'Results
                 of Detected Clusters' section will display a table showing the
-                clusters identified in each analysis area. You can download the
+                clusters detected in each analysis area. You can download the
                 output table by clicking the download button, which becomes available
                 once the scan has finished.
                 "
@@ -310,14 +325,14 @@ ui <- tagList(
             id = "sec6",
             style = "text-align: justify;",
             tags$p(tags$b("License")),
-            tags$p("This app is licensed under the GPL (>=3) license")
+            tags$p("This app is licensed under the GPL (>=3) license.")
           ),
           tags$hr(),
 
           #### Useful Resources ----
           tags$div(
             id = "sec6",
-            style = "text-align: justify;",
+            style = "text-align: justify; gap; 0 !important;",
             tags$p(tags$b("Useful Resources")),
             tags$p("Read more about:"),
             tags$ul(
